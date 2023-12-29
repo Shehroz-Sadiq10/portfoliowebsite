473b567cb70f2cea0de1775b6e95f7beee2e0cb6
Shehroz Sadiq <125815507+Shehroz-Sadiq10@users.noreply.github.com>
2023-12-29 13:26:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let type = new Typed('#changing-text', {
+    strings : ["Web Developer", "Digital Marketer", "Graphics Designer", "Youtuber"],
+    typespeed: 10,
+    backspeed: 10,
+    backdelay: 1000,
+    loop: true,
+});
+;
+
+
+
+
+
